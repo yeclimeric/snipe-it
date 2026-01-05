bd98ee62e1ed3dce6bc9963853b378449051cffb
akemidx <kojotek.dx@gmail.com>
2026-01-05 12:16:16 -0700

    something something prebreak

---
+++ app/Http/Requests/UpdateSharedTemplateRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Traits\MayContainCustomFields;
+use App\Models\ReportTemplate;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
+
+class UpdateSharedTemplateRequest extends CustomAssetReportRequest
+{
+    use MayContainCustomFields;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+            return Gate::allows('view', $this->id); //wait, no we always have the ability to view, we need to restrict on the rules
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $setting = Setting::getSettings(); //probably don't need this
+
+        $rules = array_merge(
+            parent::rules(),
+            (new ReportTemplate)->getRules(),
+                [
+                    if ($this->created_by == auth()->id())  //so how do we apply this rule. is it allow or restrict?
+
+                ],
+        )
+
+        return $rules;
+    }
+}
